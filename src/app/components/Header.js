4c3d71be1f74b1b6d31088d6ea8e3500a8b87eed
shaik-ahmed-irshad <edventurous.ahmed@gmail.com>
2024-04-24 15:10:41 +0530

    fireCool Expense NextApp: frontend done!

---
+++ src/app/components/Header.js
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+const Header = () => {
+  return (
+    <h1 style={{marginTop:'10px',textDecorationLine:'underline'}}>FireCool Expenses App</h1 >
+  )
+}
+
+export default Header
